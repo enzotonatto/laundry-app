94e28e61613f787f99ce0747ab2423038333bccb
enzotonatto <enzotonatto@gmail.com>
2025-06-09 19:38:16 -0300

    First commit

---
+++ laundry-app/Screens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  laundry-app
+//
+//  Created by Enzo Tonatto on 08/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
